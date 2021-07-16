c5922ee0364bf5a04e8df706809b1f48e666f6b3
August Emmery-Funch <32144571+mlohnrob@users.noreply.github.com>
2021-07-16 15:45:53 +0200

    divide function

--- basics/helloWorld.c
+++ basics/helloWorld.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
-/* This is a comment. */
+
+float divide(int a, int b) {
+	a = (float) a;
+	b = (float) b;
+	return a / b;
+}
+
 
 int main() {
 	printf("Hello world!\n");
+	float result = divide(40, 5);
+	printf("%f\n", result);
 	return 0;
 }
+
